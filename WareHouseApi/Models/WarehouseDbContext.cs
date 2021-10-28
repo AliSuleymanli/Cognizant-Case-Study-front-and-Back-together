14c355caaf33145ae2c2b637edd6df4e1f64ad9e
Ali Suleymanli <ali.glasstech@gmail.com>
2021-10-29 01:13:28 +0400

    connection point to sql server database is declared

---
+++ WareHouseApi/Models/WarehouseDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WareHouseApi.Models
+{
+    public class WarehouseDbContext :DbContext
+    {
+        public WarehouseDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<WareHouse> WareHouses { get; set; }
+        DbSet<Vehicle> Vehicles { get; set; } 
+    }
+}
