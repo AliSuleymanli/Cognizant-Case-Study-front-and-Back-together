ade6857ff66e87fa968ea351dfcbf66960842434
Ali Suleymanli <ali.glasstech@gmail.com>
2021-10-29 00:57:36 +0400

    domain models created

---
+++ WareHouseApi/Models/WareHouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WareHouseApi.Models
+{
+    public class WareHouse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Locationlat { get; set; }
+        public double Locationlong { get; set; }
+        public string CarLocation { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; } 
+    }
+}
